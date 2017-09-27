803f44b94885e870c34647a41581ce4750ed6ea3
Freddie Ridell <freddie@codogo.io>
2017-09-27 17:01:19 +0100

    feat(data): gather up all data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import Home from "./site/home";
+import allData from "src/content";
+import Home from "src/site/home";
+
+console.log(allData);
 
 export default () => <Home/>;
 
---
+++ src/content/index.js
@@ -0,0 +1,22 @@
+//@preval
+
+const fs = require("fs");
+
+const walkSync = (dir, filelist) => {
+
+	const files = fs.readdirSync(dir);
+
+	filelist = filelist || [];
+	files.forEach(function(file) {
+		if (fs.statSync(dir + file).isDirectory()) {
+			filelist = walkSync(dir + file + '/', filelist);
+		}
+		else {
+			filelist.push( [ dir, file, ]);
+		}
+	});
+	return filelist;
+};
+
+
+module.exports = walkSync("src/content/");
