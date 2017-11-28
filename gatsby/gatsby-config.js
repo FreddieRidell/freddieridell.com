0c0c32dbf2e5eaf6c30e7cb4ab783e6b352651aa
Freddie Ridell <freddie@codogo.io>
2017-11-28 09:29:24 +0000

    feat(boys-and-gatsby): added boys article, started hooking up gatsby

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,6 +1,16 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-  },
-  plugins: [`gatsby-plugin-react-helmet`],
+	siteMetadata: {
+		title: `Gatsby Default Starter`,
+	},
+	plugins: [
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `content`,
+				path: `${__dirname}/src/content`,
+			},
+		},
+		"gatsby-transformer-remark",
+		`gatsby-plugin-react-helmet`,
+	],
 }
--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,13 +1,39 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
+const IndexPage = ({ data, }) => (
+	console.log(data),
+	<div>
+		<h1>Hi people</h1>
+		<p>Welcome to your new Gatsby site.</p>
+		<p>Now go build something great.</p>
+		<Link to="/page-2/">Go to page 2</Link>
+	</div>
 )
 
 export default IndexPage
+
+export const query = graphql`
+	query AboutQuery {
+		site {
+			siteMetadata {
+				title
+			}
+		}
+		allMarkdownRemark {
+			edges {
+				node {
+					fileAbsolutePath
+					html
+					excerpt
+					timeToRead
+					frontmatter {
+						title
+						published
+					}
+				}
+			}
+		}
+	}
+`
+
