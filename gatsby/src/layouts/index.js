20e0e9bc17495778f828647d77efe119e5387221
Freddie Ridell <freddie@codogo.io>
2017-12-04 17:55:53 +0000

    style(gatsby): got back my dignature style

--- gatsby/src/layouts/index.js
+++ gatsby/src/layouts/index.js
@@ -1,64 +1,77 @@
 import React from 'react'
+import R from "ramda";
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
+import styled from "styled-components";
 
 import './index.css'
 
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+const ContentPane = styled.div`
+	width: 100%;
+	max-width: 30rem;
+`;
+
+const BreadcrumbContainer = styled.div`
+	flex-flow: row wrap;
+
+	& a::after {
+		content: "/";
+		padding: 0 0.33em;
+		font-weight: normal;
+		text-decoration: none;
+	}
+`;
+
+const Header = () => {
+	const breadcrumb = location.pathname
+		.split("/")
+		.filter(R.length)
+		.reduce(
+			(acc, val) => [
+				...acc,
+				{
+					path: ((R.last(acc) || {}).path || "") + "/" + val,
+					label: val,
+				},
+			],
+			[
+				{
+					path: "",
+					label: "freddie",
+				},
+			],
+		);
+
+	return (
+		<div>
+			<BreadcrumbContainer>
+				{breadcrumb.map(({ path, label, }) => [
+					<Link to = { path + "/" }>{label}</Link>,
+				])}
+			</BreadcrumbContainer>
+
+			<hr />
+		</div>
+	);
+};
 
 const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
-  </div>
+	<ContentPane>
+		<Helmet
+			title="Gatsby Default Starter"
+			meta={[
+				{ name: 'description', content: 'Sample' },
+				{ name: 'keywords', content: 'sample, something' },
+			]}
+		/>
+		<Header />
+		{children()}
+	</ContentPane>
 )
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
+	children: PropTypes.func,
 }
 
 export default TemplateWrapper
--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -39,3 +39,45 @@ export const query = graphql`
 	}
 `
 
+
+//# Hi, I'm Freddie Ridell.
+
+//+ I'm the CTO of [codogo.io](https://codogo.io).
+//+ I write about my open source work [here](/open-source/)
+//+ I've started bloging [here](/blog/)
+//+ I sometimes do arts-and-crafts [here](/crafty/)
+
+//## Social Media:
+//[twitter][twitter]
+//|
+//[github][github]
+//|
+//[refind][refind]
+//|
+//[mastodon][mastodon]
+//|
+//[freddie@codogo.io][email]
+
+//---
+
+//This page is still very much a work in progress, but I'm adding new content to it every day.
+//Please do check back in regularly
+
+//## Addresses:
+//<div id="addresses">
+//pgp:    6BE0AF404BB9D844281F3DA5ACD8B92700BF93A0
+//eth:    0x330E067E74FB15d4A71e58afFf459Db92eA85a78
+//monero: 46CFjzzLYuWJtT1tuFaRSTRp3XnqH8rUB6j8CpHgXC9EG3WBbdzfQKXZxcKNSoz28AS4hPRus7acaG5XEiEAvkkX3izgNkg
+//btc:    159CcWg7MvmKePfUoKxXYRf2TPEorGzqGD
+//ltc:    LfZdJDtwNpsKrsJYEgqAuJnCBoyiMPYs1Y
+//doge:   DEVPfEpNTeTzUMzttfdxZ9ULTUAqzY2vPw
+//Yu Gi Oh Duel Links: 549-882-525
+//</div>
+
+//[refind]: https://refind.com/FreddieRidell?invite=6ea3358605
+//[twitter]: https://twitter.com/FreddieRidell
+//[github]: https://github.com/CodogoFreddie
+//[email]: mailto:freddie@codogo.io
+//[mastodon]: https://toot.cafe/@freddieRidell
+//[pgp]: https://pgp.mit.edu/pks/lookup?op=vindex&search=0xACD8B92700BF93A0
+
