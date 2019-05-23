9b7ff68055673089fc2614e22132cf3905e19749
Freddie Ridell <contact@freddieridell.com>
2019-05-23 16:13:15 +0100

    added padding to main

--- src/components/Layout/Main.js
+++ src/components/Layout/Main.js
@@ -1,9 +1,12 @@
 import React from "react";
+import * as R from "ramda";
 import styled from "@emotion/styled";
+import { calm } from "@freddieridell/little-bonsai-styles";
 
-const MainStyled = styled.main({
+const MainStyled = styled.main(calm({
 	flex: 1,
-});
+	padding: R.path(["theme", "size", "space", 1]),
+}));
 
 const Main = ({ children }) => <MainStyled>{children}</MainStyled>;
 
