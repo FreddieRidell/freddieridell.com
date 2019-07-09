6dea0dd00888489831f9ea81f90e3c4c6f39952d
Freddie Ridell <contact@freddieridell.com>
2019-07-09 20:24:34 +0100

    made the home page look like a listing

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,32 +1,51 @@
 import React from "react";
 import * as R from "ramda";
 import styled from "@emotion/styled";
-import { createShadow } from "@freddieridell/little-bonsai-styles";
+import { calm } from "@freddieridell/little-bonsai-styles";
 
+import DividedList from "../Toolbox/DividedList";
+import HeadShot from "./HeadShot";
+import PageTitle from "../Toolbox/PageTitle";
+import Profile from "./Profile";
 import Spacer from "../Toolbox/Spacer";
 
-import HeadShot from "./HeadShot";
-import Profile from "./Profile";
+const PostHr = styled.hr(
+	calm({
+		display: "block",
+		color: R.path(["theme", "color", "symantic", "text"]),
+	}),
+);
 
-const Hero = styled.section(
-	{ display: "flex", flexWrap: "wrap", margin: "auto" },
-	R.applySpec({
+const PostDivider = () => (
+	<React.Fragment>
+		<Spacer height={4} />
+		<PostHr />
+		<Spacer height={3} />
+	</React.Fragment>
+);
+
+const PostContaner = styled.nav(
+	calm({
+		alignSelf: "center",
 		maxWidth: R.pipe(
 			R.path(["theme", "size", "paragraphWidth"]),
-
-			R.multiply(2.5),
 		),
-		boxShadow: createShadow(5),
 	}),
 );
 
 const Home = () => (
 	<React.Fragment>
-		<Hero>
-			<HeadShot />
-			<Profile />
-		</Hero>
-		<Spacer height={1} />
+		<PageTitle>Home</PageTitle>
+		<Spacer height={6} />
+		<PostContaner>
+			<DividedList
+				data={[]}
+				getKey={() => "x"}
+				renderItem={() => <div />}
+				renderDivider={() => PostDivider}
+			/>
+		</PostContaner>
+		<Spacer height={3} />
 	</React.Fragment>
 );
 
