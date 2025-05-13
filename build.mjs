9ce9740e0b78eb869546fa985cd6e48111aa223a
Freddie Ridell <contact@freddieridell.com>
2025-05-13 17:07:19 +0100

    wip 2025-05-13_17:07:19

--- build.mjs
+++ build.mjs
@@ -1,17 +1,33 @@
 import fs from "fs/promises";
-import path from "path";
+import formatBytes from "bytes";
+import { minify } from "html-minifier";
+import { gzip } from "node-gzip";
+
+function getStringBytes(s) {
+  const textEncoder = new TextEncoder();
+  return textEncoder.encode(s).length;
+}
 
 async function main(htmlSrcPath, cssSrcPath, outputPath) {
   const [htmlSrc, cssSrc] = await Promise.all(
     [htmlSrcPath, cssSrcPath].map((p) => fs.readFile(p, "utf8")),
   );
 
-  const htmlOut = htmlSrc.replace(
-    "<style />",
-    ["<style>", cssSrc, "</style>"].join(" "),
+  const htmlOut = minify(htmlSrc.replace("STYLE_REPLACE_MARKER", cssSrc), {
+    collapseWhitespace: true,
+    minifyJs: true,
+  });
+
+  const withBytes = htmlOut.replace(
+    "BYTES_INFO_MARKER",
+    `${formatBytes(getStringBytes(htmlOut), {
+      decimalPlaces: 1,
+    })}, (${formatBytes(getStringBytes(await gzip(htmlOut)), {
+      decimalPlaces: 1,
+    })} gziped)`,
   );
 
-  await fs.writeFile(outputPath, htmlOut);
+  await fs.writeFile(outputPath, withBytes);
 }
 
 main(...process.argv.slice(2));
